adfaedde7991747b5780da75ab299c558f7c6af5
Júlia Roldi <julialcantara.ra@gmail.com>
2020-08-08 19:09:12 -0300

    new ft

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
-
+import githubBackground from '../assets/githubBackground.svg';
 export default createGlobalStyle`
  *{
      margin:0;
@@ -8,8 +8,24 @@ export default createGlobalStyle`
      box-sizing:border-box;
  }
 
- box{
-     background: #F0F0F5;
+ body{
+     background: #F0F0F5 url(${githubBackground}) no-repeat 70% top;
      -webkit-font-smoothing: antialiased;
+
+    }
+
+ body, input, button {
+    font: 16px Roboto, sans-serif
  }
-`
+
+ #root {
+     max-width: 960px;
+     margin: 0 auto;
+     padding: 40px 20px;
+ }
+
+ button{
+     cursor: pointer;
+ }
+
+`;
